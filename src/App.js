ca77bd0c0cbe78c936678c2a7b424fb90118ff77
Vlad <vladislav.stan.1998@gmail.com>
2023-06-06 14:37:31 +0300

    I refactored the state storage by storing, receiving, modifying and deleting data from the server.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import { nanoid } from 'nanoid';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
@@ -7,24 +7,44 @@ import BookList from './components/BookList';
 function App() {
     const [books, setBooks] = useState([]);
 
-    const handleCreateBook = title => {
-        const updateBooks = [...books, { id: nanoid(), title }];
+    useEffect(() => {
+        fetchBooks()
+    }, [])
+
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books')
+
+        setBooks(response.data)
+    }
+
+    const handleCreateBook = async title => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
+        const updateBooks = [...books, response.data];
 
         setBooks(updateBooks);
     };
 
-    const handleDeleteBookById = id => {
+    const handleDeleteBookById = async id => {
+        await axios.delete(`http://localhost:3001/books${id}`)
+
         const updateBooks = books.filter(book => book.id !== id);
 
         setBooks(updateBooks);
     };
 
-    const handleEditBookById = (id, newTitle) => {
+    const handleEditBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books${id}`, {
+            title: newTitle
+        })
+
         const updatedBooks = books.map(book => {
             if (book.id === id) {
                 return {
                     ...book,
-                    title: newTitle,
+                    ...response.data,
                 };
             }
 
