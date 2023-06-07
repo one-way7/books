d8fb30ffd2f84d479d57f362c9f34b2e9d691310
Vlad <vladislav.stan.1998@gmail.com>
2023-06-07 10:51:06 +0300

    Implemented the ability to store and modify application state through context

--- src/context/books.js
+++ src/context/books.js
@@ -1,9 +1,64 @@
 import { useState, createContext } from 'react';
+import axios from 'axios';
 
 const BooksContext = createContext();
 
 function Provider({ children }) {
+    const [books, setBooks] = useState([]);
 
+    const fetchBooks = async () => {
+        const response = await axios.get('http://localhost:3001/books')
+
+        setBooks(response.data)
+    }
+
+    const createBook = async title => {
+        const response = await axios.post('http://localhost:3001/books', {
+            title,
+        });
+
+        const updateBooks = [...books, response.data];
+
+        setBooks(updateBooks);
+    };
+
+    const deleteBookById = async id => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
+
+        const updateBooks = books.filter(book => book.id !== id);
+
+        setBooks(updateBooks);
+    };
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        })
+
+        const updatedBooks = books.map(book => {
+            if (book.id === id) {
+                return {
+                    ...book,
+                    ...response.data,
+                };
+            }
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const data = {
+        books,
+        fetchBooks,
+        createBook,
+        deleteBookById,
+        editBookById,
+    }
+
+    return <BooksContext.Provider value={data}>{children}</BooksContext.Provider>
 }
 
+export { Provider }
 export default BooksContext;
