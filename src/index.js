a8dba7ed454ed210308dd7df38eb3d9dc25cdf0e
Vlad <vladislav.stan.1998@gmail.com>
2023-06-07 10:51:33 +0300

    wrapped project components in Provider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { Provider } from './context/books';
 import App from './App';
 
 import './index.css';
@@ -7,6 +9,6 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider><App /></Provider>
     </React.StrictMode>,
 );
